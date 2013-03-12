b5b985ae3acd1af2ccf5a85c8640740e449b1c70
jragsdale <jragsdale@phoenixwebgroup.com>
2013-03-12 13:55:16 -0500

    Fix for supplying a full MIME type, rather than just the stuff after the "image/".  Preserves backward compatibility.

--- src/HtmlTags/ImageTag.cs
+++ src/HtmlTags/ImageTag.cs
@@ -19,11 +19,17 @@
 			return this;
 		}
 
+		/// <summary>
+		/// Gets an image tag with Base64 encoding.
+		/// </summary>
+		/// <param name="mimetype">MIME Type of the image. If the supplied value does not have a slash ("/"), then the mimetype will be prepended with "image/" in the returned tag</param>
+		/// <param name="source">Contents of the image</param>
+		/// <returns>Returns an image tag with the image contents encoded in Base64</returns>
 		public ImageTag Src(string mimetype, byte[] source)
 		{
 			var base64 = Convert.ToBase64String(source, Base64FormattingOptions.None);
-			Attr(HtmlAttributeConstants.Src,
-				 string.Format("data:image/{0};base64,{1}", mimetype.ToLower(), HttpUtility.UrlEncode(base64, Encoding.Default)));
+			var imagePrefix = mimetype.Contains("/") ? string.Empty : "image/";
+			Attr(HtmlAttributeConstants.Src,string.Format("data:{0}{1};base64,{2}", imagePrefix, mimetype.ToLower(), HttpUtility.UrlEncode(base64, Encoding.Default)));
 			return this;
 		}
 
